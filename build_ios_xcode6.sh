bcc87c9e05cb56a715e79fdac043f957e89ce48d
郑朝元 <aaronzheng@kugou.net>
2016-06-27 19:26:40 +0800

    fix 'bitcode bundle could not be generated because xxx was built without full bitcode'

--- build_ios_xcode6.sh
+++ build_ios_xcode6.sh
@@ -1,63 +1,80 @@
 #!/bin/sh
-###modified from http://blog.csdn.net/chaoyuan899/article/details/51701100
 
 set -xe
 
-DEVELOPER=`xcode-select -print-path`
-DEST=`pwd .`"/libfdk-aac"
+VERSION="0.1.3"
+SDKVERSION="9.2"
+LIBSRCNAME="fdk-aac-ios"
 
-ARCHS="x86_64 armv7 armv7s arm64 i386"
+CURRENTPATH=`pwd`
+
+mkdir -p "${CURRENTPATH}/src"
+cd "${CURRENTPATH}/"
+
+DEVELOPER=`xcode-select -print-path`
+DEST="${CURRENTPATH}/lib-ios"
+mkdir -p "${DEST}"
+
+ARCHS="armv7 armv7s arm64 i386 x86_64"
+# ARCHS="armv7"
 LIBS="libfdk-aac.a"
 
-mkdir -p $DEST
-
-./configure
+DEVELOPER=`xcode-select -print-path`
 
 for arch in $ARCHS; do
-        make clean
-        IOSMV=" -miphoneos-version-min=5.0"
-        case $arch in
-        arm*)
-                if [ $arch == "arm64" ]
-                then
-                        IOSMV=" -miphoneos-version-min=7.0"
-                fi
-                echo "Building opencore-amr for iPhoneOS $arch ****************"
-                PATH=`xcodebuild -version -sdk iphoneos PlatformPath`"/Developer/usr/bin:$PATH" \
-                SDK=`xcodebuild -version -sdk iphoneos Path` \
-		CC="xcrun --sdk iphoneos clang -arch $arch $IOSMV --sysroot=$SDK -isystem $SDK/usr/include"\
-                CXX="xcrun --sdk iphoneos clang++ -arch $arch $IOSMV --sysroot=$SDK -fembed-bitcode-marker -isystem $SDK/usr/include" \
-                LDFLAGS="-Wl,-syslibroot,$SDK" \
-                ./configure \
-                --host=arm-apple-darwin \
-                --prefix=$DEST \
-                --disable-shared
-                ;;
-        *)
-                echo "Building opencore-amr for iPhoneSimulator $arch *****************"
-                PATH=`xcodebuild -version -sdk iphonesimulator PlatformPath`"/Developer/usr/bin:$PATH" \
-		SDKROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
-		CC="$(xcrun --sdk iphonesimulator -f clang)"
-		CXX="xcrun --sdk iphonesimulator clang++ -arch $arch -fembed-bitcode-marker $IOSMV -miphoneos-version-min=5.0" \
-                ./configure \
-                --prefix=$DEST \
-                --disable-shared
-                ;;
-        esac
-	make -j3
-        make install
-        for i in $LIBS; do
-                mv $DEST/lib/$i $DEST/lib/$i.$arch
-        done
-done
+case $arch in
+arm*)
 
-echo "Merge into universal binary."
+IOSV="-miphoneos-version-min=7.0"
+if [ $arch == "arm64" ]
+then
+IOSV="-miphoneos-version-min=7.0"
+fi
+
+echo "Building for iOS $arch ****************"
+SDKROOT="$(xcrun --sdk iphoneos --show-sdk-path)"
+CC="$(xcrun --sdk iphoneos -f clang)"
+CXX="$(xcrun --sdk iphoneos -f clang++)"
+CPP="$(xcrun -sdk iphonesimulator -f clang++)"
+CFLAGS="-isysroot $SDKROOT -arch $arch $IOSV -isystem $SDKROOT/usr/include -fembed-bitcode"
+CXXFLAGS=$CFLAGS
+CPPFLAGS=$CFLAGS
+export CC CXX CFLAGS CXXFLAGS CPPFLAGS
+
+./configure \
+--host=arm-apple-darwin \
+--prefix=$DEST \
+--disable-shared --enable-static
+;;
+*)
+IOSV="-mios-simulator-version-min=7.0"
+echo "Building for iOS $arch*****************"
+
+SDKROOT=`xcodebuild -version -sdk iphonesimulator Path`
+CC="$(xcrun -sdk iphoneos -f clang)"
+CXX="$(xcrun -sdk iphonesimulator -f clang++)"
+CPP="$(xcrun -sdk iphonesimulator -f clang++)"
+CFLAGS="-isysroot $SDKROOT -arch $arch $IOSV -isystem $SDKROOT/usr/include -fembed-bitcode"
+CXXFLAGS=$CFLAGS
+CPPFLAGS=$CFLAGS
+export CC CXX CFLAGS CXXFLAGS CPPFLAGS
+./configure \
+--prefix=$DEST \
+--disable-shared
+;;
+esac
+make > /dev/null
+make install
+make clean
+for i in $LIBS; do
+mv $DEST/lib/$i $DEST/lib/$i.$arch
+done
+done
 
 for i in $LIBS; do
-        input=""
-        for arch in $ARCHS; do
-                input="$input $DEST/lib/$i.$arch"
-        done
-        xcrun lipo -create -output $DEST/lib/$i $input
+input=""
+for arch in $ARCHS; do
+input="$input $DEST/lib/$i.$arch"
 done
-
+lipo -create -output $DEST/lib/$i $input
+done
